8b5925a3f10cdf8018f0684e8112de4da25aee6e
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-02 17:52:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,11 @@
  **main - Main function of my project
  **Return: return 0 everytime
  **/
+
 int main(void)
 {
 	char displayed[52];
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
+	strcpy(displayed, "\"Programming is like a multilingual puzzle\n");
+	puts (displayed);
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  **main - Main function
  **Return: return 0 everytime
