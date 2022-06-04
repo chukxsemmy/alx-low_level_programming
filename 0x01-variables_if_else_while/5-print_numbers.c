83cf5ed5d5ee59b53df81f40e80876572c13dd81
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-03 18:06:45 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ **main - Prints all single digits of base 10 followed by new line
+ **
+ **Return: returns 0
+**/
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n)
+			;
+	}
+	putchar('\n');
+	return (0);
+}
