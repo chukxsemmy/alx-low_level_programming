6bf0eafe4f225e2174e47211bfdbb756ff1cd565
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-07 08:10:40 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *times_table - prints 9 times table starting from zero
+ *Returns: 0
+ *@n: takes the value of numbers
+ */
+
+void times_table(void n)
+{
+	int times_table, n;
+
+	for (times_table = 0; times_table <= 9; times_table++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			n = n * n;
+			_putchar(n)
+		}
+		times_table = times_table * times_table;
+		_putchar(times_table);
+	}
+	return (0);
+}
