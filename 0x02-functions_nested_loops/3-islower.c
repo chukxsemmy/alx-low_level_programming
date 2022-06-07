5118e37f0ad939dea67c40ae0fe361ddd676e5f9
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-07 06:19:26 -0700

    checks for small letter c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *int _islower - checks for lowercase letter
+ *_islower - checks for lowercase letter
  *
  *Return: 1 or 0
  *@c: letter to be verified
  */
 
-int int _islower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
