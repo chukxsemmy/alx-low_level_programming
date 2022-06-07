db4cb07a8b8d51ab8d797724fa6b8ed2ae96a2f2
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-07 13:33:38 -0700

    adds two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,14 +3,16 @@
 /**
  *add - sums up the value of two numbers or digits
  *Return: 0
- *@int: takes the value of integers
+ *@x: takes the value of the first integer
+ *@y: takes the value of the second integer
  */
 
-int add(int, int)
+int add(int x, int y)
 {
-	int x, y, sum;
+	int sum;
 
-	sum = add(x, y);
+	sum = (x + y);
+	_putchar(sum);
 	return (0);
 }
 
