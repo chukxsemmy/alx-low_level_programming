c9d6e224627e5b9519dc8225cd49ec6c606d5759
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-07 05:20:20 -0700

    print the letters of the alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints letters of the alphabets ten times
+ *
+ *Return: none
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	int count;
+
+	while (count++ > 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+			_putchar(10);
+		}
+	}
+}
