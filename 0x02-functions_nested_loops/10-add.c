18789a904c2ca69e4402194f11da6073536a2e27
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-07 13:28:31 -0700

    adds the value of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - sums up the value of two numbers or digits
+ *Return: 0
+ *@int: takes the value of integers
+ */
+
+int add(int, int)
+{
+	int x, y, sum;
+
+	sum = add(x, y);
+	return (0);
+}
+
