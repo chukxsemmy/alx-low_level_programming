036f0e572dacc999834dab6dbec8e04c760df4fa
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-09 12:01:56 -0700

    priniting character diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ **print_diagonal - prints a diagonal line of the slanted character
+ **@n:holds the value for the lines
+ **pos: position to print the character
+ **dis: distance between the begining line and the charcater
+ **Return: none
+ **/
+
+void print_diagonal(int n)
+{
+	int pos, dis;
+
+	if (n > 0)
+	{
+		for (dis = 0; dis < n; dis++)
+		{
+			for (pos = 0; pos < len; pos++)
+				_putchar(' ');
+			_putchar('\\');
+			if (len == (n - 1))
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
