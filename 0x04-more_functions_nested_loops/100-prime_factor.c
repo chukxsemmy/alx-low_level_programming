4dbc2e341c838c9d3f91ba3fd6888a2800a4ef7e
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-09 14:31:13 -0700

    printing largest prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ **main- printing the lagest prime number
+ **n takes the value of the number lagest prime number
+ **num takes the value of the number
+ **Return: returns 0
+ **/
+
+int main(void)
+{
+	long num = 612852475143;
+	int n;
+
+	while (num > n / 2)
+	{
+		if (num % n == 0)
+		{
+			num /= 2;
+			continue;
+		}
+		for (n = 3; n < num / 2; n += 2)
+		{
+			if (num % n == 0)
+				num /= n;
+		}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
