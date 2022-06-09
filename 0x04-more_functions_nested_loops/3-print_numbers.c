ffebce6b8fea23592552af488dd957de55f87fd8
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-09 11:28:30 -0700

    printing numbers 1 to 10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ **print_numbers - printing number 1 to 9.
+ **Return: 0
+ **/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n > 10; n++)
+		_putchar(n);
+	_putchar(10);
+}
