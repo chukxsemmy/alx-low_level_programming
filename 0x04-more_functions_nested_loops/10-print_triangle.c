341287e62094e7aea0d76055f75de5818ea1f3f3
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-09 14:19:30 -0700

    printing of triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include <std.io>
+
+/**
+ *print_triangle - printing a triangle
+ *@size: takes the size of the trangle
+ *hgt takes the height of the triangle
+ *wid takes the width of the trangle
+ *Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int hgt, wid;
+
+	if (size > 0)
+	{
+		for (hgt = 0; hgt <= size; hgt++)
+		{
+			for ((wid = size - hgt); wid > 0; wid--)
+				_putchar(' ');
+			for (wid = 0; wid < hgt; wid++)
+				_putchar('#');
+			if (hgt == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
