97cbeb8609a5868c0da6dc2acb9c29f9f80ac907
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-09 16:38:07 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n > 9)
+	if (num > 9)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(num % 10 + '0');
 }
