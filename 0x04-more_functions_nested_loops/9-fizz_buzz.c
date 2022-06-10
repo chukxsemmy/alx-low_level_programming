bdd618c4cd443ce3d4cd8e1186594b11ca70b8bb
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-09 17:18:59 -0700

    fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,29 @@ int main(void)
 {
 	int n;
 
-	while (n++ <= 100)
-		if ((n % 3) == 0)
-			printf("Fizz ");
-		else if ((n % 5) == 0)
-			printf("Buzz ");
-		else if ((n % 5 == 0) && (n % 3 == 0))
-			printf("FizzBuzz ");
+	for (n = 1; a <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
 		else
-			printf("%d ", n);
+		{
+			printf("%d", n);
+		}
+		if (n != 100)
+		{
+			printf(" ");
+		}
+	}
 	printf("\n");
 	return (0);
 }
