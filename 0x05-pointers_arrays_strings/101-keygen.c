8deb431a08192e280494037d5daaa6c98c187712
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-13 12:35:45 -0700

    generating a random number

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - generating a random password
+ *Return: the password
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	srand(time(0));
+	while (a <= 2645)
+	{
+		b = rand() % 128;
+		a += b;
+		putchar(c);
+	}
+	putchar(2772 - a);
+	return (0);
+}
