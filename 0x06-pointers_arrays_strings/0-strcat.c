c250a430a0188a9886f9c537580a871f36d5927b
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-15 15:04:19 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		len++;
+		dest_len++;
 	for (index = 0; src[index]; index++)
-		dest[len++] = src[index];
+		dest[dest_len++] = src[index];
 	return (dest);
 }
