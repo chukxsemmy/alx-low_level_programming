3999bb870132f863ca6681e0c1d972a0c4a18e2d
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-16 20:56:19 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,12 @@
 
 void print_number(int n)
 {
-	unsigned int x;
-
-	x = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -n;
+		n = -n;
 	}
-	if (x / 10 != 0)
-	{
-		print_number(x / 10);
-	}
-	_putchar((x % 10) + '0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
 }
