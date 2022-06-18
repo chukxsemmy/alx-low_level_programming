da03bd2409901cb286d203c54513c0e821d85dfc
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-18 03:45:11 -0700

    remove putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 void print_line(char *c, int s, int l);
 int _putchar(char);
-int putchar(char);
