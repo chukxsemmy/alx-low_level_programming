d187bb5f12db5fdd2a7ecdc404f6e7e770485b97
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-22 14:20:08 -0700

    exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - returns the value of x exponent y
+ *@x: base number
+ *@y: exponent
+ *Return: x exponent y or -1 if y greater than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
