bd5b7989852d720186b0c27328b363dbd16a68fe
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-22 16:46:31 -0700

    fix main

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
-/**
- *prototypes
- */
-
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -16,9 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _sqrt(int n, int x);
-int is_prime(int n, int start);
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-
-#endif /* MAIN_H */
+char *wildcard(char *s3, char *s4);
