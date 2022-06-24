96297433df50c8015bdf62333d73c994778ee38c
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-24 13:33:48 -0700

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main class
+ * @argc: count argumnets passed to a funtion
+ * @argv: argument vector of pointer to string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
