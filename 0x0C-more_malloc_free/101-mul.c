137a58dcabdefe5e3679542499375f461dbff342
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-01 02:57:49 +0800

    betty fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,26 +9,24 @@ void add_nums(char *final_prod, char *next_prod, int next_len);
 /**
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
- *
  * Return: The length of the string.
  */
+
 int find_len(char *str)
 {
 	int len = 0;
 
 	while (*str++)
 		len++;
-
 	return (len);
 }
 
 /**
  * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
+ * the character 'x'. Adds a terminating null byte.
  * @size: The size of the array to be initialized.
- *
  * Description: If there is insufficient space, the
- *              function exits with a status of 98.
+ * function exits with a status of 98.
  * Return: A pointer to the array.
  */
 char *create_xarray(int size)
@@ -37,41 +35,36 @@ char *create_xarray(int size)
 	int index;
 
 	array = malloc(sizeof(char) * size);
-
 	if (array == NULL)
 		exit(98);
-
 	for (index = 0; index < (size - 1); index++)
 		array[index] = 'x';
-
 	array[index] = '\0';
-
 	return (array);
 }
 
 /**
  * iterate_zeroes - Iterates through a string of numbers containing
- *                  leading zeroes until it hits a non-zero number.
+ * leading zeroes until it hits a non-zero number.
  * @str: The string of numbers to be iterate through.
- *
  * Return: A pointer to the next non-zero element.
  */
+
 char *iterate_zeroes(char *str)
 {
 	while (*str && *str == '0')
 		str++;
-
 	return (str);
 }
 
 /**
  * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
- *
  * Description: If c is a non-digit, the function
- *              exits with a status of 98.
+ * exits with a status of 98.
  * Return: The converted int.
  */
+
 int get_digit(char c)
 {
 	int digit = c - '0';
@@ -81,7 +74,6 @@ int get_digit(char c)
 		printf("Error\n");
 		exit(98);
 	}
-
 	return (digit);
 }
 
@@ -91,10 +83,10 @@ int get_digit(char c)
  * @mult: The string of numbers.
  * @digit: The single digit.
  * @zeroes: The necessary number of leading zeroes.
- *
  * Description: If mult contains a non-digit, the function
- *              exits with a status value of 98.
+ * exits with a status value of 98.
  */
+
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
 	int mult_len, num, tens = 0;
@@ -107,9 +99,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 		*prod = 'x';
 		prod++;
 	}
-
 	prod--;
-
 	while (zeroes--)
 	{
 		*prod = '0';
@@ -182,7 +172,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  * @argc: An array of pointers to the arguments.
  *
  * Description: If the number of arguments is incorrect or one number
- *              contains non-digits, the function exits with a status of 98.
+ * contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
