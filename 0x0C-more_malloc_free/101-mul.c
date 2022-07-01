5fb308f4218b7e201aed81550f218b5b66c01862
Chukwuma Emmanuel <chukxsemmy@gmail.com>
2022-06-30 20:20:52 -0700

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -146,7 +146,6 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 		num += tens;
 		*final_prod = (num % 10) + '0';
 		tens = num / 10;
-
 		next_prod--;
 		next_len--;
 	}
@@ -157,11 +156,9 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 		num += tens;
 		*final_prod = (num % 10) + '0';
 		tens = num / 10;
-
 		final_prod--;
 		next_prod--;
 	}
-
 	if (tens)
 		*final_prod = (tens % 10) + '0';
 }
@@ -185,7 +182,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	if (*(argv[1]) == '0')
 		argv[1] = iterate_zeroes(argv[1]);
 	if (*(argv[2]) == '0')
@@ -199,7 +195,6 @@ int main(int argc, char *argv[])
 	size = find_len(argv[1]) + find_len(argv[2]);
 	final_prod = create_xarray(size + 1);
 	next_prod = create_xarray(size + 1);
-
 	for (index = find_len(argv[2]) - 1; index >= 0; index--)
 	{
 		digit = get_digit(*(argv[2] + index));
@@ -212,9 +207,7 @@ int main(int argc, char *argv[])
 			putchar(final_prod[index]);
 	}
 	putchar('\n');
-
 	free(next_prod);
 	free(final_prod);
-
 	return (0);
 }
