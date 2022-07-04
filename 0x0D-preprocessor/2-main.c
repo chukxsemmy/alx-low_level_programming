ad09fef6caaa13a6410b00a6114492f8e5b4ec6b
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-05 05:38:16 +0800

    printing name of file complied from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printing the name of name of file
+ * compiled from
+ *
+ * Return: nothing to be returned
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
