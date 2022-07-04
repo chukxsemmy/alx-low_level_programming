3bbb2b16bdec0dfe63cfc015ea569c09405d749d
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-05 06:04:19 +0800

    main header fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,18 +5,26 @@
 #include <stdlib.h>
 
 /**
-*struct dog - a new type of data
-*representing a dog
-*@name: name of dog
-*@age: age of dog
-*@owner: owner's name
-*/
+ * struct dog - a new type of data
+ * representing a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's name
+ */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -24,6 +32,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _strlen(char *str);
 char *_strcopy(char *dest, char *src);
-
 
 #endif
