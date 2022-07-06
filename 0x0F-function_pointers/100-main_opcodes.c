024229c334a635b6786c580a1085d11d93e40dca
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-07 01:30:25 +0800

    generates opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints opcode of own main function
+ *
+ * @argc: argument count
+ * @argv: arg value
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	unsigned char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	c = (unsigned char *)main;
+	b = 0;
+	if (a > 0)
+	{
+		while (b < (a - 1))
+			printf("%02hhx ", c[b++]);
+		printf("%hhx\n", c[b]);
+	}
+	return (0);
+}
