2c6d1fb530cb17efbdda3515d59dcbc6fa0f10eb
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-07 08:23:14 +0800

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,28 +6,32 @@
  * main - performs simple operations
  * @argc: arguement count
  * @argv: arguement value
- * Return: int
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int a, b, c;
-	int (*fun)(int, int);
+	int num1, num2;
+	char *cal;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	fun = get_op_func(argv[2]);
-	if (fun == NULL)
+	num1 = atoi(argv[1]);
+	cal = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(cal) == NULL || cal[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	c = fun(a, b);
-	printf("%d\n", c);
+	if ((*cal == '/' && num2 == 0) || (*cal == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
