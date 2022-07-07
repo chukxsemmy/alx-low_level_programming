517b235ff6bddd7dad3750bea6d5478fed407aaa
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-07 08:32:19 +0800

    simple ops

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,24 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
- * op_add - add
- * @a:integer
- * @b:intger
- * Return:result
- */
+*op_add - Returns the sum of two numbers
+*@a:the first number
+*@b:the second number
+*
+*Return:The sum of a and b
+*/
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+
 /**
- * op_sub - sub
- * @a:integer
- * @b:intger
- * Return:result
- */
+*op_sub - Returns the substraction of two numbers
+*@a:The first number
+*@b:The second number
+*
+*Return: The Difference of a and b
+*/
 
 int op_sub(int a, int b)
 {
@@ -27,47 +31,41 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - mul
- * @a:integer
- * @b:intger
- * Return:result
- */
+*op_mul - Returns the multiplication of two numbers
+*@a:The first number
+*@b: The second number
+*
+*Return: The multiplication of a and b
+*/
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+
 /**
- * op_div - div
- * @a:integer
- * @b:intger
- * Return:result
- */
+*op_div - Returns the division of two numbers.
+*@a: The first number.
+*@b: The second number.
+*
+*Return: The quotient of a and b.
+*/
 
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
 /**
- * op_mod - mod
- * @a:integer
- * @b:intger
- * Return:result
- */
+*op_mod - Returns the remainder of the division of two numbers.
+*@a: The first number.
+*@b: The second number.
+*
+*Return: The remainder of the division of a by b.
+*/
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
