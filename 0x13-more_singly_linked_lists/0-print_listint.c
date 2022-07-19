b25ff202d81fede3251ca315392de3bb1b3aa39a
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-19 11:59:26 +0800

    fix for list all elemts of linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		i += 1;
+		num += 1;
 		h = h->next;
 	}
-	return (i);
+
+	return (num);
 }
