8e390a62a7246370206c13417a4e5382e2942b8c
Chukxsemmy <chukxsemmy@gmail.com>
2022-07-20 10:31:20 +0800

    finds a loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,30 +3,31 @@
 /**
  * find_listint_loop - finds the loop in a linked list
  * @head: linked list to search for
+ *
  * Return: address of the node where the loop starts, or NULL
  */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *h1 = head;
-	listint_t *h2 = head;
+	listint_t *ptr_one = head;
+	listint_t *ptr_two = head;
 
 	if (!head)
 		return (NULL);
 
-	while (h2 && h1 && h1->next)
+	while (ptr_one && ptr_two && ptr_two->next)
 	{
-		h1 = h1>next->next;
-		h2 = h2->next;
-		if (h1 == h2)
+		ptr_two = ptr_two->next->next;
+		ptr_one = ptr_one->next;
+		if (ptr_two == ptr_one)
 		{
-			h2 = head;
-			while (h2 != h2)
+			ptr_one = head;
+			while (ptr_one != ptr_two)
 			{
-				h2 = h2->next;
-				h1 = h1->next;
+				ptr_one = ptr_one->next;
+				ptr_two = ptr_two->next;
 			}
-			return (h1);
+			return (ptr_two);
 		}
 	}
 
