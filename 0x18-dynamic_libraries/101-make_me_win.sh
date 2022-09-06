f29774352cdf913c4e41282c64468bcf6ca7ccc5
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-06 11:47:03 +0800

    giga inject

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O -P /run/giga_inject.so https://github.com/chukxsemmy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga_inject.so
+wget -q -O /run/giga_inject.so https://github.com/chukxsemmy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga_inject.so
 export LD_PRELOAD =/run/giga_inject.so
